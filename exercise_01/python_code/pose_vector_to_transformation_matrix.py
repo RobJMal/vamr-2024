049c91cf8d47ebd25a708ee3834abb61a5db49f8
Robert Jomar Malate <robjmal@gmail.com>
2024-09-26 17:02:24 +0200

    Implemented poseVectorToTransformationMatrix function

--- exercise_01/python_code/pose_vector_to_transformation_matrix.py
+++ exercise_01/python_code/pose_vector_to_transformation_matrix.py
@@ -11,5 +11,19 @@ def pose_vector_to_transformation_matrix(pose_vec: np.ndarray) -> np.ndarray:
     Returns:
         T: 4x4 transformation matrix
     """
-    pass
-    # TODO: Your code here
+    axis_angle_rotation_vec = pose_vec[:3]
+    translational_vec = pose_vec[3:]
+
+    # Applying Rodrigues' formula to get the rotation matrix
+    theta = np.linalg.norm(axis_angle_rotation_vec)
+    k = axis_angle_rotation_vec / theta
+    k_cross_product_matrix = np.array([[0, -k[2], k[1]],
+                                       [k[2], 0, -k[0]],
+                                       [-k[1], k[0], 0]])
+    rotation_matrix = np.eye(3) + np.sin(theta) * k_cross_product_matrix + (1 - np.cos(theta)) * k_cross_product_matrix @ k_cross_product_matrix
+
+    transformation_matrix : np.ndarray = np.eye(4)
+    transformation_matrix[0:3, 0:3] = rotation_matrix
+    transformation_matrix[0:3, 3] = translational_vec
+
+    return transformation_matrix 
