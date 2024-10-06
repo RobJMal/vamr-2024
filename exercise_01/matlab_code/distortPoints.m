3b447d8a83dbe89ea49b5c6810d25b4dcadbc01d
Robert Jomar Malate <robjmal@gmail.com>
2024-10-06 21:43:32 +0200

    Changed structure to colcon workspace structure

--- exercise_01/matlab_code/distortPoints.m
+++
@@ -1,7 +0,0 @@
-function x_d = distortPoints(x, D, K)
-% Applies lens distortion D(2x1) to 2D points x(2xN) on the image plane.
-
-% TODO: Your code here
-
-end
-
--- exercise_01/matlab_code/poseVectorToTransformationMatrix.m
+++
@@ -1,7 +0,0 @@
-function T = poseVectorToTransformationMatrix(pose_vec)
-% Converts a 6x1 pose vector into a 4x4 transformation matrix
-
-% TODO: Your code here
-
-end
-
