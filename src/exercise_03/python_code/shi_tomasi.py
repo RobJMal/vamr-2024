6a388d0a668613c0025ffa9c09a4d1c6163b3322
Robert Jomar Malate <robjmal@gmail.com>
2024-12-02 22:09:40 +0100

    Added exercise 03 files

---
+++ src/exercise_03/python_code/shi_tomasi.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import signal
+
+
+def shi_tomasi(img, patch_size):
+    """ Returns the shi-tomasi scores for an image and patch size patch_size
+        The returned scores are of the same shape as the input image """
+
+    pass
+    # TODO: Your code here
+
