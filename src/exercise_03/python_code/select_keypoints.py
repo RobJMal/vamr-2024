1b7d106785a8e4f3072eed7b9399bd0c6a6facef
Robert Jomar Malate <robjmal@gmail.com>
2024-12-02 22:02:39 +0100

    Adding exercises 3-9

---
+++ src/exercise_03/python_code/select_keypoints.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def selectKeypoints(scores, num, r):
+    """
+    Selects the num best scores as keypoints and performs non-maximum supression of a (2r + 1)*(2r + 1) box around
+    the current maximum.
+    """
+    pass
+    # TODO: Your code here
